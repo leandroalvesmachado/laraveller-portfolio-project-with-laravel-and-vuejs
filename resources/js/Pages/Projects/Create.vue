e40278cb07e47ce1bfcd8dc31ae1f42e0863f091
leandroalvesmachado@gmail.com <leandroalvesmachado@gmail.com>
2022-11-06 22:10:28 -0300

    Fim da aula 07

---
+++ resources/js/Pages/Projects/Create.vue
@@ -0,0 +1,27 @@
+<template>
+  <Head title="New Project" />
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        New Project
+      </h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex justify-end m-2 p-2">
+          
+        </div>
+      </div>
+    </div>
+  </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<style>
+
+</style>
